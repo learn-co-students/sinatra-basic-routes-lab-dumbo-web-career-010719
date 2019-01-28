9457cea49aa93fbcc41649493b25baf388b09846
edwinlin <edwinlin@gmail.com>
2019-01-28 10:44:57 -0500

    Done.

--- app.rb
+++ app.rb
@@ -5,25 +5,16 @@ class App < Sinatra::Base
 	get '/name' do
 	resp = Rack::Response.new
 		resp.write "My name is __"
- 
-  		# erb :'names/index.html.erb'
-  		# resp.finish
 	end
 
 	get '/hometown' do
 	resp = Rack::Response.new
 		resp.write "My hometown is __"
- 
-  		# erb :'names/index.html.erb'
-  		# resp.finish
 	end
 
 	get '/favorite-song' do
 	resp = Rack::Response.new
 		resp.write "My favorite song is __"
- 
-  		# erb :'names/index.html.erb'
-  		# resp.finish
 	end
 
 end
