7aee3c2563fd76836fe46eaac36c258283e38d83
edwinlin <edwinlin@gmail.com>
2019-01-28 10:46:31 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+	resp = Rack::Response.new
 
 	get '/name' do
-	resp = Rack::Response.new
 		resp.write "My name is __"
 	end
 
 	get '/hometown' do
-	resp = Rack::Response.new
+	# resp = Rack::Response.new
 		resp.write "My hometown is __"
 	end
 
 	get '/favorite-song' do
-	resp = Rack::Response.new
+	# resp = Rack::Response.new
 		resp.write "My favorite song is __"
 	end
 
