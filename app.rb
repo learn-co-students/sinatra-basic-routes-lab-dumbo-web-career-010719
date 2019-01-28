8769cd0e4b78303013f190e0a21a18e1b4f48aac
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-28 10:15:14 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get('/name'){
+        "My name is wei"
+    }
+
+    get('/hometown'){
+        "My hometown is ??"
+    }
+
+    get('/favorite-song'){
+        "My favorite song is ??"
+    }
 end
+
